7244bb4b9e585a776ac118602aaec5c21f002139
Reynaldo GT <larz.dev@gmail.com>
2024-01-22 15:14:45 -0400

    added new pages

--- src/app/translate_page/[title]/[phare]/page.tsx
+++ src/app/translate_page/[title]/[phare]/page.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params: { phrase: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const phrase = params.phrase
+
+  return {
+    title: 'BraillEasy',
+
+    openGraph: {
+      images: [
+        'https://www.gob.mx/cms/uploads/article/main_image/30006/braille3.jpg',
+      ],
+      description: `Asi se escribe ${params.phrase} en Braille`,
+      url: `https://braillease-git-main-larzlrael.vercel.app//`,
+    },
+  }
+}
 
 export default function PhraseTranslatePage() {
-  return (
-    <div>PhraseTranslatePage</div>
-  )
+  return <div>PhraseTranslatePage</div>
 }
